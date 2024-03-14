ee3ee3352db75ef118f9a127c82d6e509ae35781
Kooale325 <muhammad.bilal832017@gmail.com>
2024-03-14 15:58:08 +0500

    parallax test done

--- js/test.js
+++ js/test.js
@@ -1,10 +1,18 @@
-const images = document.getElementsByClassName("parallax");
-
+const images = document.querySelectorAll(".parallax");
+function limitNumberWithinRange(num, min, max) {
+  const MIN = min ?? 1;
+  const MAX = max ?? 20;
+  const parsed = parseInt(num);
+  return Math.min(Math.max(parsed, MIN), MAX);
+}
+let xValue = 0,
+  yValue = 0;
 window.addEventListener("mousemove", (mouse) => {
-  var mX = mouse.offsetX - window.screen.availWidth / 2;
-  console.log(mouse.offsetY);
-  var mY = mouse.offsetY;
-  Array.from(images).forEach((el) => {
-    el.style.transform = `translateX(${mX * 0.1}%)`;
+  xValue = limitNumberWithinRange(mouse.clientX - window.innerWidth / 2, -500, 500);
+  yValue = limitNumberWithinRange(mouse.clientY - window.innerHeight / 2, -500, 500);
+  images.forEach((image) => {
+    let speedx = image.dataset.speedx;
+    let speedy = image.dataset.speedy;
+    image.style.transform = `translate(${-xValue * speedx * 0.025}%) translateY(${-yValue * speedy * 0.025}%)`;
   });
 });
