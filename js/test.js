629aa72bce07802326d4214e1ca9813c50d1d5b8
Kooale325 <muhammad.bilal832017@gmail.com>
2024-03-14 16:03:48 +0500

    fixed animation easing

--- js/test.js
+++ js/test.js
@@ -1,15 +1,9 @@
 const images = document.querySelectorAll(".parallax");
-function limitNumberWithinRange(num, min, max) {
-  const MIN = min ?? 1;
-  const MAX = max ?? 20;
-  const parsed = parseInt(num);
-  return Math.min(Math.max(parsed, MIN), MAX);
-}
 let xValue = 0,
   yValue = 0;
 window.addEventListener("mousemove", (mouse) => {
-  xValue = limitNumberWithinRange(mouse.clientX - window.innerWidth / 2, -500, 500);
-  yValue = limitNumberWithinRange(mouse.clientY - window.innerHeight / 2, -500, 500);
+  xValue = mouse.clientX - window.innerWidth / 2;
+  yValue = mouse.clientY - window.innerHeight / 2;
   images.forEach((image) => {
     let speedx = image.dataset.speedx;
     let speedy = image.dataset.speedy;
