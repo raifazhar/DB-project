4d22825480dfd96c73e306601578d932d3b2fae6
ZtheKey1263 <126020201+ZtheKey1263@users.noreply.github.com>
2024-04-02 23:14:21 +0500

    Pushing customer support (chat page)
    Please work

---
+++ customerSupport/frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Chatbox from "../components/Chatbox"
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+
+            <p>Your website content</p>
+            <Chatbox />
+        </div>
+    )
+}
