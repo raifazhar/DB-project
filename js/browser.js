fbb83860a14f22c44ed4a9bee7a39c365266a3ee
Kooale325 <muhammad.bilal832017@gmail.com>
2024-05-04 23:59:12 +0500

    websockets initial implementation done. Message sending and recieving working

--- js/browser.js
+++ js/browser.js
@@ -1,16 +1,13 @@
-
 // url = "http://localhost:3000";
 
-fetch("http://localhost:3000" + "/api/browser",{
-    method: "GET",
-    headers:{"Content-Type": "application/json;charset=UTF-8"}
-})
-.then(async(response)=>{
-    if(response.status==200)
-    {
-          reorganizebrowser(response)
-    }
-})
+fetch("http://localhost:3000" + "/api/browser", {
+  method: "GET",
+  headers: { "Content-Type": "application/json;charset=UTF-8" },
+}).then(async (response) => {
+  if (response.status == 200) {
+    reorganizebrowser(response);
+  }
+});
 
 const searchbutton = document.querySelector("#search");
 searchbutton.addEventListener("click", function () {
@@ -18,48 +15,44 @@ searchbutton.addEventListener("click", function () {
   search(inputValue);
 });
 
-async function search(string)
-{
-    console.log(string)
-    fetch("http://localhost:3000" + "/api/search?searchstring=" + string,{
-      method: "GET",
-      headers:{"Content-Type": "application/json;charset=UTF-8"}
-    })
-    .then(async(response)=>{
-        if(response.status==200)
-        {
-            reorganizebrowser(response)
-        }
-    })
-    
-}    
-
-async function reorganizebrowser(response)
-{
-    const browsercontainer=document.querySelector('#grid-container')
-    while (browsercontainer.hasChildNodes()) {
-        browsercontainer.removeChild(browsercontainer.firstChild);
-      }
-      
-    let responsejson = await response.json();
-    console.log(responsejson);
-    responsejson.forEach((element) => {
-        var browserli = document.createElement('li');
-        var browsertitle =document.createElement('div');
-        browsertitle.classList.add('broswercell-textcontainer');
-        browsertitle.innerHTML=`<a href="" class="browsercell-title">${element.title}</a>`
-        var browserimg=document.createElement('img');
-        browserimg.setAttribute('src',element.url);
-        browserimg.classList.add('browsercell-image')
-        browserli.appendChild(browsertitle)
-        browserli.appendChild(browserimg)
-        browserli.setAttribute('data-picture-id',element.pictureID);
-        browserli.addEventListener('click',function(){getpage(element.pictureID)})
-        browsercontainer.appendChild(browserli)
-    });
+async function search(string) {
+  console.log(string);
+  fetch("http://localhost:3000" + "/api/search?searchstring=" + string, {
+    method: "GET",
+    headers: { "Content-Type": "application/json;charset=UTF-8" },
+  }).then(async (response) => {
+    if (response.status == 200) {
+      reorganizebrowser(response);
+    }
+  });
 }
-function getpage(id)
-{
-    console.log(id)
-    window.location.href = "../Place-Page/Place.html?id=" + id;
+
+async function reorganizebrowser(response) {
+  const browsercontainer = document.querySelector("#grid-container");
+  while (browsercontainer.hasChildNodes()) {
+    browsercontainer.removeChild(browsercontainer.firstChild);
+  }
+
+  let responsejson = await response.json();
+  console.log(responsejson);
+  responsejson.forEach((element) => {
+    var browserli = document.createElement("li");
+    var browsertitle = document.createElement("div");
+    browsertitle.classList.add("broswercell-textcontainer");
+    browsertitle.innerHTML = `<a href="" class="browsercell-title">${element.title}</a>`;
+    var browserimg = document.createElement("img");
+    browserimg.setAttribute("src", element.url);
+    browserimg.classList.add("browsercell-image");
+    browserli.appendChild(browsertitle);
+    browserli.appendChild(browserimg);
+    browserli.setAttribute("data-picture-id", element.pictureID);
+    browserli.addEventListener("click", function () {
+      getpage(element.pictureID);
+    });
+    browsercontainer.appendChild(browserli);
+  });
+}
+function getpage(id) {
+  console.log(id);
+  window.location.href = "../Place-Page/Place.html?id=" + id;
 }
---
+++ js/chat.js
@@ -0,0 +1,87 @@
+url = "https://db-project-api.vercel.app";
+url = "http://localhost:3000";
+
+let users = [];
+let messages = [];
+selectedUser = null;
+let loaders = document.getElementsByClassName("loaderdiv");
+let userID = parseInt(localStorage.getItem("id"));
+const queryParams = { token: localStorage.getItem("token") };
+const socket = io(url, {
+  path: "/api/socket.io",
+  query: queryParams,
+  reconnection: false,
+});
+//When we get all users we refresh user list
+socket.on("all users", (user) => {
+  users = user;
+  selectedUser = users[0].UserID;
+  for (let i = 0; i < loaders.length; i++) {
+    loaders[i].style.display = "none";
+  }
+  UpdateUsers();
+});
+
+//When we recieve message we check if its from the current user or ourselves and if it is we add it to the messages
+socket.on("recieve message", (msgobj) => {
+  if (msgobj.from !== selectedUser && msgobj.from !== userID) return;
+  messages.push({
+    id: messages.length,
+    user: msgobj.from,
+    message: msgobj.msg,
+  });
+  UpdateMessages();
+});
+
+function SendMessage() {
+  const message = document.getElementById("m").value;
+  let msgobj = {
+    from: userID,
+    to: selectedUser,
+    msg: message,
+  };
+  socket.emit("send message", msgobj);
+}
+
+function UpdateUsers() {
+  try {
+    const usersList = document.getElementById("userlist");
+    usersList.replaceChildren();
+    users.forEach((user) => {
+      const userElement = document.createElement("li");
+      userElement.textContent = user.name;
+      if (user.UserID === selectedUser) {
+        userElement.style.backgroundColor = "gray";
+      }
+      userElement.addEventListener("click", () => {
+        selectedUser = user.UserID;
+        messages = [];
+        UpdateMessages();
+        UpdateUsers();
+      });
+      usersList.appendChild(userElement);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function UpdateMessages() {
+  try {
+    const messagesList = document.getElementById("messagelist");
+    messagesList.replaceChildren();
+    messages.forEach((message) => {
+      const messageElement = document.createElement("li");
+      messageElement.textContent = message.message;
+      if (message.user !== userID) {
+        messageElement.style.backgroundColor = "gray";
+      } else {
+        messageElement.style.backgroundColor = "darkgray";
+        messageElement.style.alignSelf = "flex-end";
+      }
+      messagesList.appendChild(messageElement);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
