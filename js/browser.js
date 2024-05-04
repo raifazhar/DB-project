8eee340611f6d8617dafcf5acd76b68f18189ff3
raifazhar <raifazhar.612@gmail.com>
2024-05-04 10:37:51 +0500

    front of browser

--- js/browser.js
+++ js/browser.js
@@ -1,6 +1,15 @@
 
 url = "http://localhost:3000";
 console.log(url)
+
+const searchbutton=document.querySelector('#search')
+searchbutton.addEventListener('click',function()
+    {
+        const inputValue = document.getElementById("searchtext").value;
+        search(inputValue)
+    }
+)
+
 fetch(url + "/api/browser",{
     method: "GET"
     
@@ -8,25 +17,66 @@ fetch(url + "/api/browser",{
 .then(async(response)=>{
     if(response.status==200)
     {
-            const browsercontainer=document.querySelector('#grid-container')
-            let responsejson = await response.json();
-            
-            responsejson.forEach((element) => {
-                var browserli = document.createElement('li');
-                var browsertitle =document.createElement('div');
-                browsertitle.classList.add('broswercell-textcontainer');
-                browsertitle.innerHTML=`<a href="" class="browsercell-title">${element.title}</a>`
-                var browserimg=document.createElement('img');
-                browserimg.setAttribute('src',element.url);
-                browserimg.classList.add('browsercell-image')
-                console.log(element.title)
-                console.log(element.url)
-                browserli.appendChild(browsertitle)
-                browserli.appendChild(browserimg)
-                browserli.setAttribute('id',responsejson.pictureID);
-                console.log(browserli)
-                browsercontainer.appendChild(browserli)
-            });
-            
+          reorganizebrowser(response)
+    }
+})
+
+
+
+function search(string)
+{
+    console.log(string)
+    fetch("http://localhost:3000" + "/api/search?searchstring=" + string,{
+      method: "GET",
+      headers:{"Content-Type": "application/json;charset=UTF-8"}
+    })
+    .then(async(response)=>{
+        if(response.status==200)
+        {
+            reorganizebrowser(response)
         }
     })
+    
+}    
+
+async function reorganizebrowser(response)
+{
+    const browsercontainer=document.querySelector('#grid-container')
+    while (browsercontainer.hasChildNodes()) {
+        browsercontainer.removeChild(browsercontainer.firstChild);
+      }
+      
+    let responsejson = await response.json();
+    console.log(responsejson);
+    responsejson.forEach((element) => {
+        var browserli = document.createElement('li');
+        var browsertitle =document.createElement('div');
+        browsertitle.classList.add('broswercell-textcontainer');
+        browsertitle.innerHTML=`<a href="" class="browsercell-title">${element.title}</a>`
+        var browserimg=document.createElement('img');
+        browserimg.setAttribute('src',element.url);
+        browserimg.classList.add('browsercell-image')
+        browserli.appendChild(browsertitle)
+        browserli.appendChild(browserimg)
+        console.log(element.pictureID)
+        browserli.setAttribute('data-picture-id',element.pictureID);
+        browserli.addEventListener('click',function(){getpage(element.pictureID)})
+        console.log(browserli)
+        browsercontainer.appendChild(browserli)
+    });
+}
+// function getpage(id)
+// {
+//     fetch(url+"/api/palace",
+//     {
+//         method:"GET",
+//         body:{pictureID:id}
+//     })
+//     .then(async(response)=>{
+//         if(response.status==200)
+//         {
+//             let responsejson = await response.json();
+//         }
+//     })
+//     console.log(id);
+// }
