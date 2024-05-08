b969f2699b39572c5d0110e36de7945fcda668b9
raifazhar <raifazhar.612@gmail.com>
2024-05-09 00:17:53 +0500

    place-page-mostlydone

--- js/place.js
+++ js/place.js
@@ -30,18 +30,10 @@ fetch(url + "/api/placepage?pageid=" + 1, {
 const right = document.querySelector(".right");
 const left = document.querySelector(".left");
 const container = document.querySelectorAll(".image");
+const title=document.querySelector(".title")
 currentIndex = 0;
 activeindex = 0;
-// container.forEach((element ,index) => {
 
-//     element.addEventListener("click", function () {
-//         container.forEach(element => {element.classList.remove("active")})
-//         element.classList.add("active");
-//         activeindex=index
-//         overidebackground(element)
-//         index++;
-//       });
-// });
 
 for (let j = 0; j < container.length; j++) {
   container[j].addEventListener("click", function () {
@@ -50,49 +42,53 @@ for (let j = 0; j < container.length; j++) {
     });
     container[j].classList.add("active");
     activeindex = j;
-    overidebackground(container[j]);
+    moveelementleft()
+    moveelementright()
+    overidebackground(container[j])
   });
 }
 right.addEventListener("click", function () {
-  moveelementright(1);
+  moveelementright();
+  if (activeindex < container.length - 1) {
+    container[activeindex].classList.remove("active");
+    activeindex++;
+    container[activeindex].classList.add("active");
+    overidebackground(container[activeindex]);
+  }
 });
 left.addEventListener("click", function () {
-  moveelementleft(1);
+  moveelementleft();
+  if (activeindex >= 1) {
+    console.log(container[activeindex])
+    container[activeindex].classList.remove("active");
+    activeindex--;
+    container[activeindex].classList.add("active");
+    console.log(container[activeindex])
+    overidebackground(container[activeindex]);
+  }
 });
 
 function overidebackground(element) {
   const imgChild = element.querySelector("img");
+  const titleelement=element.querySelector("h3")
   document.body.style.background = `url(${imgChild.getAttribute("src")}) no-repeat top/cover`;
+  console.log(titleelement)
+  console.log(title)
+  title.textContent=titleelement.textContent;
 }
-function moveelementright(times) {
-  for (let i = 0; i < times; i++) {
-    if (currentIndex < container.length - 4 && activeindex - currentIndex >= 1) {
-      container[currentIndex].classList.remove("visible");
+function moveelementright() {
+  if (currentIndex < container.length - 4 && activeindex - currentIndex >= 1) {
+    container[currentIndex].classList.remove("visible");
 
-      currentIndex++;
-      container[currentIndex + 3].classList.add("visible");
-    }
-    if (activeindex < container.length - 1) {
-      container[activeindex].classList.remove("active");
-      activeindex++;
-      container[activeindex].classList.add("active");
-      overidebackground(container[activeindex]);
-    }
+    currentIndex++;
+    container[currentIndex + 3].classList.add("visible");
   }
+  
 }
-function moveelementleft(times) {
-  for (let i = 0; i < times; i++) {
-    if (currentIndex >= 1 && activeindex - currentIndex < 2) {
-      container[currentIndex + 3].classList.remove("visible");
-
-      currentIndex--;
-      container[currentIndex].classList.add("visible");
-    }
-    if (activeindex >= 1) {
-      container[activeindex].classList.remove("active");
-      activeindex--;
-      container[activeindex].classList.add("active");
-      overidebackground(container[activeindex]);
-    }
-  }
+function moveelementleft() {
+  if (currentIndex >= 1 && activeindex - currentIndex < 2) {
+    container[currentIndex + 3].classList.remove("visible");
+    currentIndex--;
+    container[currentIndex].classList.add("visible");
+  }  
 }
